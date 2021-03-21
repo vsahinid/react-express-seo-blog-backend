2fda49e7fc71eb572cda0c3409cadf082118122b
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-20 22:32:16 -0400

    Added User model

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { time } = require("../controllers/blog");
 
-router.get("/", time);
+router.get("/blogs", time);
 
 module.exports = router;
