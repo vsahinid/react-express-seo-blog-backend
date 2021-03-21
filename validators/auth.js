a9680332c61a317497654ddbdbd91662a9865032
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-20 22:47:44 -0400

    Added validators

--- validators/auth.js
+++ validators/auth.js
@@ -2,7 +2,7 @@ const { check } = require("express-validator");
 
 exports.userSignupValidator = [
   check("name").not().isEmpty().withMessage("Name is required"),
-  check("email").isEmail().withMessage("Must be a valid email"),
+  check("email").isEmail().withMessage("Must be a valid email address"),
   check("password")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters long"),
