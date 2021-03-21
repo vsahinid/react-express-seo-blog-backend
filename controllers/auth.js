458e6ae6cc9c00f5818ff5dd1d8f63660abfb5d0
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-21 15:00:50 -0400

    Added all rooutes

--- controllers/auth.js
+++ controllers/auth.js
@@ -71,6 +71,8 @@ exports.signout = (req, res) => {
 
 exports.requireSignin = expressJwt({
   secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"], // added later
+  userProperty: "auth",
 });
 
 exports.authMiddleware = (req, res, next) => {
