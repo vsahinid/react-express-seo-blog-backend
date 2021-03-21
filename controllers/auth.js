769658c2d20adbce233a15ec51ddb7956902c025
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-21 13:55:39 -0400

    Added category model, user and admin routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,3 +61,47 @@ exports.signin = (req, res) => {
     });
   });
 };
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "Signout success",
+  });
+};
+
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+});
+
+exports.authMiddleware = (req, res, next) => {
+  const authUserId = req.user._id;
+  User.findById({ _id: authUserId }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+    req.profile = user;
+    next();
+  });
+};
+
+exports.adminMiddleware = (req, res, next) => {
+  const adminUserId = req.user._id;
+  User.findById({ _id: adminUserId }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+
+    if (user.role !== 1) {
+      return res.status(400).json({
+        error: "Admin resource. Access denied",
+      });
+    }
+
+    req.profile = user;
+    next();
+  });
+};
