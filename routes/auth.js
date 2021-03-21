95ec52ee2f837a8ca510b57a24746f26775262d9
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-21 13:39:01 -0400

    Added more protected routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin } = require("../controllers/auth");
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin,
+} = require("../controllers/auth");
 
 // validators
 const { runValidation } = require("../validators");
@@ -11,5 +16,12 @@ const {
 
 router.post("/signup", userSignupValidator, runValidation, signup);
 router.post("/signin", userSigninValidator, runValidation, signin);
+router.get("/signout", signout);
+// test
+router.get("/secret", requireSignin, (req, res) => {
+  res.json({
+    message: "you have access to secret page",
+  });
+});
 
 module.exports = router;
